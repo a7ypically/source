5c3ba148f33cc924c8492f7dc0751f9ddfa25397
a7ypically <a7ypically@gmail.com>
2016-12-01 22:17:43 +0000

    Update list

--- pkg_inst.sh
+++ pkg_inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop https-dns-proxy lm-sensors mtr redsocks rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp https_dns_proxy
+./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp

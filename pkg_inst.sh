30f5b5be2104637c8d33bacf409f3228c3f83221
a7ypically <a7ypically@gmail.com>
2016-12-01 17:00:10 +0000

    Add http-dns-proxy

--- pkg_inst.sh
+++ pkg_inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop https-dns-proxy lm-sensors mtr redsocks rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp
+./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop https-dns-proxy lm-sensors mtr redsocks rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp https_dns_proxy

52bb443787d7d2b1de66ce6036e7b57de21dc250
a7ypically <a7ypically@gmail.com>
2016-12-02 14:10:26 +0000

    Add missing packages.

--- pkg_inst.sh
+++ pkg_inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp
+./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp libcares libev

1a41f8c6cf3b32ccb8df47428c07e3d3ebb0517e
a7ypically <a7ypically@gmail.com>
2016-12-03 15:28:24 +0000

    Fix compilation for x86

--- pkg_inst.sh
+++ pkg_inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp libcares libev
+./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp libcares libev alsa-lib lame libx264 opus speex libv4l libdrm

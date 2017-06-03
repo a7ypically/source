f7bb103386887a440760e60e31a9dd3b69876ef5
a7ypically <a7ypically@gmail.com>
2017-06-03 18:13:54 +0300

    Add packages.

--- pkg_inst.sh
+++ pkg_inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp libcares libev alsa-lib lame libx264 libopus libspeex libv4l libdrm arp-scan sysstat
+./scripts/feeds install luci bc ddns-scripts diffutils ffmpeg htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp libcares libev alsa-lib lame libx264 libopus libspeex libv4l libdrm arp-scan sysstat screen

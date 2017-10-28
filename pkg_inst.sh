6b3cd82bcab97a460d5727ce90e5936b04251787
a7ypically <a7ypically@gmail.com>
2017-10-28 12:18:38 +0300

    Add network stats pks.

--- pkg_inst.sh
+++ pkg_inst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./scripts/feeds install luci bc ddns-scripts diffutils htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp luci-app-sqm libcares libev arp-scan sysstat screen bind-dig
+./scripts/feeds install luci bc ddns-scripts diffutils htop lm-sensors mtr rsync sqm-scripts vim luci-app-ddns luci-app-openvpn luci-app-upnp luci-app-sqm libcares libev arp-scan sysstat screen bind-dig luci-app-nlbwmon
